00b9351b7bc5d5be3aae05977d60489cf944bbdf
Roberto <rrh2023@bu.edu>
2026-03-21 16:41:19 -0400

    changed model

--- server/models/Event.js
+++ server/models/Event.js
@@ -27,8 +27,8 @@ const eventSchema = new mongoose.Schema(
     },
     category: {
       type: String,
-      enum: ['poetry', 'visual-arts', 'music', 'community', 'experimental'],
-      default: 'community',
+      enum: ['music', 'art', 'food', 'sports', 'community', 'nightlife', 'culture', 'other'],
+      required: true,
     },
     capacity: {
       type: Number,
